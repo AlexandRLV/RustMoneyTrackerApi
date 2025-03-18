0ef56dd03004e3877b907a4a5246d4d465227c19
Aleksandr Litskevich <avlitskevich@gmail.com>
2025-03-19 00:06:20 +0500

    Added auth cookie check

--- src/error.rs
+++ src/error.rs
@@ -5,7 +5,7 @@ pub type Result<T> = core::result::Result<T, Error>;
 #[derive(Debug)]
 pub enum Error {
     LoginFail,
-
+    AuthFailNoAuthTokenCookie,
     TicketDeleteFailIdNotFound { id: u64 },
 }
 
